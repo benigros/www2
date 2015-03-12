17268192df25c48c1faf4962bd2b0795a8154d99
benigros <bdevilliers@ensc.fr>
2015-03-12 13:10:52 +0100

    Update RechercheGroupe.php

--- RechercheGroupe.php
+++ RechercheGroupe.php
@@ -2,3 +2,8 @@
 include("head.php");
 ?>
         <h1> Recherche de groupe</h1>
+		
+	<label for="Nom_Groupe">Nom du groupe</label>: <input type="text" name="Nom_Groupe" id="Nom_Groupe" value="" /><br/><br/>
+	
+	<?php include("foot.php") ;?>
+	</html>
